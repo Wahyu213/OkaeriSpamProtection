30443661524eb21fdb91d4f253c6666c6dfe29d7
Wahyusaputra <91831925+Wahyu213@users.noreply.github.com>
2021-12-23 15:22:54 +0700

    Changed API url again

---
+++ spr/__init__.py
@@ -0,0 +1,34 @@
+from os.path import exists
+from sqlite3 import connect
+
+from aiohttp import ClientSession
+from pyrogram import Client
+from Python_ARQ import ARQ
+
+SESSION_NAME = "spr"
+DB_NAME = "db.sqlite3"
+API_ID = 6
+API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
+ARQ_API_URL = "https://thearq.tech"
+
+if exists("config.py"):
+    from config import *
+else:
+    from sample_config import *
+
+session = ClientSession()
+
+arq = ARQ(ARQ_API_URL, ARQ_API_KEY, session)
+
+conn = connect(DB_NAME)
+
+spr = Client(
+    SESSION_NAME,
+    bot_token=BOT_TOKEN,
+    api_id=API_ID,
+    api_hash=API_HASH,
+)
+with spr:
+    bot = spr.get_me()
+    BOT_ID = bot.id
+    BOT_USERNAME = bot.username
